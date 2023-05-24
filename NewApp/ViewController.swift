7ecc148451369c0653f2942abe03d333af925c4a
Geon_27 <yukgeon98527@gmail.com>
2023-05-24 22:34:37 +0900

    Initial Commit

---
+++ NewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewApp
+//
+//  Created by 육건 on 2023/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
