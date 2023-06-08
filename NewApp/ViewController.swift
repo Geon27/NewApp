e48eef156cbb06dfc06e565725dc21bd8afe6f2a
Geon_27 <yukgeon98527@gmail.com>
2023-06-09 00:11:44 +0900

    23/06/09 code added

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -9,5 +9,7 @@ class ViewController: UIViewController {
     @IBOutlet var emailField: UITextField!
     @IBOutlet var passwordField: UITextField!
     @IBOutlet var login: UIButton!
+    // 상황에 따라 다른 alert를 줄 때 바꿔줄 문장을 tmpMessage로 설정
+        var tmpMessage: String = ""
 }
 
