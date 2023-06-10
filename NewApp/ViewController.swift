2f88b195dcd47d3d149cf373a5e0a286db7a8b3e
Geon_27 <yukgeon98527@gmail.com>
2023-06-10 10:52:01 +0900

    23/06/10 code added

--- NewApp/ViewController.swift
+++ NewApp/ViewController.swift
@@ -4,6 +4,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        login.layer.cornerRadius = 10
     }
     @IBOutlet var mainLabel: UILabel!
     @IBOutlet var emailField: UITextField!
@@ -11,5 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet var login: UIButton!
     // 상황에 따라 다른 alert를 줄 때 바꿔줄 문장을 tmpMessage로 설정
         var tmpMessage: String = ""
+
 }
 
